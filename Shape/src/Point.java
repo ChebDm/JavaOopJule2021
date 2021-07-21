299f0ad3f0872f6de95fb059ed6a5592f07ec0db
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2021-07-21 09:11:42 +0700

    add Point class

---
+++ Shape/src/Point.java
@@ -0,0 +1,37 @@
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getDistance(Point point) {
+        return Math.sqrt(Math.pow((x - point.x), 2) + Math.pow((y - point.y), 2));
+    }
+}
